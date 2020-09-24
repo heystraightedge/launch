acb4d76fedf4089006ce24d874cadc79008c21c0
Sunny Aggarwal <sunnya97@protonmail.ch>
2020-09-24 04:49:33 -0400

    webpack

--- site_in_client/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-// can we even get this to import
-import mnemonicGenerate from './node_modules/@polkadot/util-crypto/mnemonic/generate.js';
-
-function foo() {
-    console.log("hello"); // Prints: Hello World!
-    alert("hi");
-  }
---
+++ site_in_client/src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+// can we even get this to import
+import mnemonicGenerate from '@polkadot/util-crypto/mnemonic/generate.js';
+
+window.foo = function foo() {
+  console.log("hello"); // Prints: Hello World!
+  alert("hi");
+}
